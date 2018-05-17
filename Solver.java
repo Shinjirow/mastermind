c6c52fb268ec9d14df9341e4e05562455f9307a9
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-05-18 01:30:37 +0900

    update comment

--- Solver.java
+++ Solver.java
@@ -1,31 +1,68 @@
 import java.util.Arrays;
 import java.util.Map;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.Collections;
 
-public class Solver {
+/**
+ * Solver
+ * マスターマインドに回答するクラス
+ */
+public class Solver extends Object {
 
+	/**
+	 * デバッグ用変数
+	 */
 	private static boolean DEBUG = true;
 
+	/**
+	 * 回答のチェック回数の上限値
+	 */
 	private static int limit = MasterMind.getlimit();
+	
+	/**
+	 * マスターマインドの問題の桁数
+	 */
 	private static int zigen = MasterMind.getzigen();
-	private static int[] x = new int[zigen];
+	
+	/**
+	 * 回答をチェックした時のレスポンスを束縛する変数
+	 */
 	private static int[] hint = new int[2];
-	private static int[] bestAnswer = new int[zigen];
-	private static int[] bestscore = { 0, 0 };
-
+	
+	/**
+	 * 回答提出用の変数
+	 */
 	private static int[] testAnswer = new int[zigen];
+	
+	/**
+	 * 0 ~ 9で、どの値が何度出現したかを記録しておく連想配列
+	 * 途中でソートして挿入順に取り出されることを保証したいのでLinkedHashMapである
+	 */
 	private static Map<Integer, Integer> frequency = new LinkedHashMap<Integer, Integer>();
-	private static int[] prevHint = new int[2];
-	private static int[] prevAnswer = new int[zigen];
+	
+	/**
+	 * 1つ手前の提出でヒットした回数を代入しておく変数
+	 */
 	private static int prevHit;
+
+	/**
+	 * 1つ手前の提出でその桁に入ってた値
+	 */
 	private static int prevDigit;
 
+	/**
+	 * 提出回数を記録しておく変数.
+	 */
 	private static int submission = 0;
 
+	/**
+	 * マスターマインドに回答するメインメソッド
+	 * 前処理10回で連想配列にどれが何度出現するかを記録しておき、
+	 * 次に各桁の値を順番に決めていく
+	 * この時、提案する順番を出現頻度の高い順にしていくことにより、
+	 * O(桁数 * 各桁がとりうる値)からO(1/2 * 桁数 * 各桁がとりうる値の種類数)になってるはず
+	 */
 	public static void answer() {
 		Solver.countEachValue();
 
@@ -34,24 +71,35 @@ public class Solver {
 		Solver.submit();
 	}
 
-	private static void countEachValue(){
+	/**
+	 * 前処理10回を行うメソッド
+	 * 全てi(0 <= i < 10)で埋めて提出しヒットの数を測る
+	 */
+	private static void countEachValue() {
 		for(int i = 0;i < 10;i++){
 			Arrays.fill(testAnswer, i);
 			Solver.evaluateFirst(i);
 		}
+
+		return;
 	}
 
-	private static void decideNumber(){
+	/**
+	 * 実際に正解を求めていくメソッド
+	 * そんなに改良してないので遅い
+	 * N=桁数, M=それぞれの桁が取りうる値の範囲とすると
+	 * 提出回数はO(1/2 * N * M)であるはず
+	 * JavaのソートがO(NlogN)なら
+	 * 計算量自体はO(1/2 * N * M * MlogM)
+	 */
+	private static void decideNumber() {
 		for(int i = 0;i < zigen;i++){
 			Solver.sortFrequency();
 
 			int j = 0;
 			for(Map.Entry<Integer, Integer> entry : frequency.entrySet()){
-				// if(DEBUG) System.err.println("entry = " + entry);
 				testAnswer[i] = entry.getKey();
-				// if(DEBUG) {System.err.print("Submission = ");for(int k : testAnswer) System.err.print(k); System.err.println();}
 				if(Solver.evaluateSecond(i, j)) {
-					// if(DEBUG) System.err.println("わかったので減らす");
 					frequency.put(testAnswer[i], frequency.get(testAnswer[i]) - 1);
 					if(frequency.get(testAnswer[i]) == 0) frequency.remove(testAnswer[i]);
 					break;
@@ -60,45 +108,59 @@ public class Solver {
 			}
 		}
 
-		// if(DEBUG) frequency.entrySet().forEach(System.err::println);
+		return;
 	}
 
-	private static void evaluateFirst(int i){
+	/**
+	 * Frequencyテーブルを作成する前処理段階での評価メソッド
+	 * 一旦提出して帰ってきた値が0じゃなかったら、
+	 * 連想配列に追加する
+	 * @param i : int 提出する際に埋めた値
+	 */
+	private static void evaluateFirst(int i) {
 		Solver.submission++;
 		hint = MasterMind.evaluate(testAnswer);
 		if(hint[0]!=0) frequency.put(i, hint[0]);
 		if(hint[0] == zigen) Solver.submit();
+
+		return;
 	}
 
-	private static boolean evaluateSecond(int digit, int index){
+	/**
+	 * 各桁ごとに値を変えたあと、実際に評価するメソッド
+	 * ちょっといい加減な実装なので余計な評価をさせている可能性がある
+	 * @param digit : int 今どの桁を求めているか
+	 * @param index : int その桁について、今何度目の提出か(0-indexed)
+	 * @return その桁の値がわかった場合true
+	 */
+	private static boolean evaluateSecond(int digit, int index) {
 		Solver.submission++;
 		hint = MasterMind.evaluate(testAnswer);
 		if(hint[0] == zigen) Solver.submit();
-		// if(DEBUG) {for(int i : hint) {System.err.print(i);} System.err.println();}
 		if(index > 0){
-			// if(DEBUG) System.err.println("prevHit = " + prevHit);
 			if(index == 1){
 				if(prevHit > hint[0]){
-					//0回目の提案が正しかった
-					testAnswer = prevAnswer;
 					testAnswer[digit] = prevDigit;
 					return true;
 				}
 			}
 			if(prevHit < hint[0]){
-				//n回目の提案が正しかった
 				return true;
 			}
 		}
 
-		prevHint = hint;
 		prevHit = hint[0];
-		prevAnswer = testAnswer;
 		prevDigit = testAnswer[digit];
 		return false;
 	}
 
-	private static void sortFrequency(){
+	/**
+	 * 出現頻度テーブルをソートするメソッド
+	 * 連想配列のvalueを降順にソートする
+	 * JavaのMapインタフェースは挿入順に取り出すことを保証していないので、
+	 * ポリモーフィズムを失うあまりよくないプログラムだがLinkedHashMapに指定している
+	 */
+	private static void sortFrequency() {
 		frequency = frequency.entrySet().stream()
 		.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
 		.collect(Collectors.toMap(
@@ -107,10 +169,18 @@ public class Solver {
 			(s,a) -> a,
 			LinkedHashMap::new
 		));
+
+		return;
 	}
 
-	private static void submit(){
+	/**
+	 * 実際にファイナルアンサーを行うメソッド
+	 * DEBUG変数がtrueだった場合、それまでに提出した回数を標準エラー出力に出力する
+	 */
+	private static void submit() {
 		if(DEBUG) System.err.println("submission = " +  submission);
 		MasterMind.submit(testAnswer);
+
+		return;
 	}
 }
